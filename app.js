33ff9c8e7d9b21048132b0385940440cd68122a3
WilliamAndry <164076206+WilliamAndry@users.noreply.github.com>
2024-06-18 16:18:58 -0600

    mejora basica al JavaScript.js
    Se anexo el uso del return, cola manera correcta de llamar datos desde el Js  y el identificador al la etiqueta

--- app.js
+++ app.js
@@ -1,9 +1,21 @@
-let titulo = document.querySelector("h1");
-titulo.innerHTML="Juego del número secreto";
+let numeroSecreto = generarNumeroSecreto();
+console.log(numeroSecreto);
 
-let parrafo = document.querySelector("p");
-
-
-function intentoDeUsuario(){
-    alert('Click desde el boton')
+function asignarTextoElemento(elemento,texto){
+    let elementoHTML = document.querySelector(elemento);
+    elementoHTML.innerHTML=texto;
+    return;
 }
+    
+function verificarIntento(){
+    let numeroDeUsuario = parseInt(document.getElementById('valorUsuario').value);
+    console.log(numeroDeUsuario);
+    return;
+}
+  
+function generarNumeroSecreto(){
+    return Math.floor(Math.random()*10)+1
+}
+
+asignarTextoElemento("h1","Juego del número secreto")
+asignarTextoElemento("p","Indica un número del 1 al 100")

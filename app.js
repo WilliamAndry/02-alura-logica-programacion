551f81f7ce400b563c4869448857f6c67856b5c6
WilliamAndry <164076206+WilliamAndry@users.noreply.github.com>
2024-06-18 12:59:39 -0600

    Vinculo de HTML con JS y primera funcion

---
+++ app.js
@@ -0,0 +1,9 @@
+let titulo = document.querySelector("h1");
+titulo.innerHTML="Juego del número secreto";
+
+let parrafo = document.querySelector("p");
+
+
+function intentoDeUsuario(){
+    alert('Click desde el boton')
+}
